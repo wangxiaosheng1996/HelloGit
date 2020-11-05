1d64ce1e7b6c1eabfd247196ad0f9acf32c520d0
wxs_glb <1440668719_glb@qq.com>
2020-11-05 20:41:13 +0800

    分支提交

--- src/main/java/com/scut/HelloWorld.java
+++ src/main/java/com/scut/HelloWorld.java
@@ -8,6 +8,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("第一次提交");
         System.out.println("第二次提交");
+        System.out.println("分支提交");
 
     }
 }
